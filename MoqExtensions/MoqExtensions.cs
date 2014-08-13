c7fa0638a1c3dfbd827a612002ffda188c2438a2
gwest <garrywest@gmail.com>
2014-08-13 11:55:35 +0100

    Added the project.

---
+++ MoqExtensions/MoqExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace MoqExtensions
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    using Moq.Language.Flow;
+
+    public static class MoqExtensions
+    {
+        public static void ReturnsInOrder<T, TResult>(this ISetup<T, TResult> setup,
+          params TResult[] results) where T : class
+        {
+            setup.Returns(new Queue<TResult>(results).Dequeue);
+        }
+
+        public static void ReturnsInOrder<T, TResult>(this ISetup<T, TResult> setup, params object[] results) where T : class
+        {
+            var queue = new Queue(results);
+            setup.Returns(() =>
+            {
+                var result = queue.Dequeue();
+                if (result is Exception)
+                {
+                    throw result as Exception;
+                }
+                return (TResult)result;
+            });
+        }
+    }
+}
